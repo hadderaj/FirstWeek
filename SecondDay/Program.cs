f1a5615b649288fa9de9689a4337ebdc19513331
hadderaj <77488231+hadderaj@users.noreply.github.com>
2021-01-19 10:29:41 +0100

    Initial commit

---
+++ SecondDay/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SecondDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+            int heltal = 123;
+            Console.WriteLine(heltal);
+
+        }
+
+
+
+    }
+}
